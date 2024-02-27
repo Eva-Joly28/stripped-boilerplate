1bba73521cc36075005f37069e15f1b1a2dfe391
Eva-Joly28 <fabitakedo@gmail.com>
2024-02-27 11:24:23 +0100

    avec le backend

---
+++ Backend/src/routes.ts
@@ -0,0 +1,43 @@
+import { TaskController } from "./controller/TaskController"
+import { UserController } from "./controller/UserController"
+
+export const Routes = [
+{
+    method: "get",
+    route: "/tasks",
+    controller: TaskController,
+    action: "getAllTasks"
+},
+
+{
+    method: "get",
+    route: "/tasks/:id",
+    controller: TaskController,
+    action: "getTaskById"
+},
+
+{
+    method: "post",
+    route: "/tasks",
+    controller: TaskController,
+    action: "addTask"
+},
+{
+    method: "patch",
+    route: "/tasks/:id",
+    controller: TaskController,
+    action: "updateTask"
+}, 
+{
+    method: "delete",
+    route: "/tasks/:id",
+    controller: TaskController,
+    action: "deleteById"
+},
+{
+    method: "delete",
+    route: "/tasks",
+    controller: TaskController,
+    action: "deleteAll"
+}
+]
